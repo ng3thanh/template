c35ff36e1601efef5bf743bf8a72ce93bfd7d3ca
Thanh Nguyen <Thanh.NguyenBa@Nashtechglobal.com>
2018-07-17 14:22:26 +0700

    update code

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -225,7 +225,7 @@
                                 <a href="#" class="btn btn-default btn-flat">Profile</a>
                             </div>
                             <div class="pull-right">
-                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                <a href="{{ route('auth.logout') }}" class="btn btn-default btn-flat">Sign out</a>
                             </div>
                         </li>
                     </ul></li>
