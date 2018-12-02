f33193bb6493bf4143670907365353e6362250a8
ng3thanh <ngthanh2093@gmail.com>
2018-12-02 22:06:31 +0700

    update

--- resources/views/admin/pages/products/create.blade.php
+++ resources/views/admin/pages/products/create.blade.php
@@ -8,78 +8,94 @@
 
 @section('content')
     <section class="content">
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                <h3 class="box-title">Create new product</h3>
-            </div>
+        <form role="form"
+              id="create-new-product"
+              class="form-horizontal"
+              action="{{ route('product.store') }}"
+              method="POST"
+              enctype="multipart/form-data">
 
-            <div class="box-body with-border">
-                <form role="form"
-                      id="create-new-product"
-                      class="form-horizontal"
-                      action="{{ route('product.store') }}"
-                      method="POST"
-                      enctype="multipart/form-data">
-
-                    {{ csrf_field() }}
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label" for="title-en"> Title <span class="span-red">*</span></label>
-                            <div class="col-sm-9">
-                                <input type="text" id="title-en" name="trans[en][title]" data-rule-required="true" maxlength="200" class="form-control" placeholder="Title ..." value="{{ old('trans.en.title') }}">
-                                @include('elements.error_line', ['attribute' => 'trans.en.title'])
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label" for="slug-en"> Slug <span class="span-red">*</span></label>
-                            <div class="col-sm-9">
-                                <input type="text" id="slug-en" name="trans[en][slug]" data-rule-required="true" maxlength="200" class="form-control" value="{{ old('trans.en.slug') }}">
-                                @include('elements.error_line', ['attribute' => 'trans.en.slug'])
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label" for="des_ckediter"> Description <span class="span-red">*</span></label>
-                            <div class="col-sm-9">
-                                <textarea class="form-control" id="des_ckediter" name="trans[en][description]" maxlength="1000" data-rule-required="true" rows="4" cols="80">{{ old('trans.en.description') }}</textarea>
-                                @include('elements.error_line', ['attribute' => 'trans.en.description'])
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label" for="content_ckediter"> Content <span class="span-red">*</span></label>
-                            <div class="col-sm-9">
-                                <textarea class="form-control" id="content_ckediter" name="trans[en][content]" maxlength="20000" rows="10" cols="80">{{ old('trans.en.content') }}</textarea>
-                                @include('elements.error_line', ['attribute' => 'trans.en.content'])
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label" for="tag-en"> Tag <span class="span-red">*</span></label>
-                            <div class="col-sm-9">
-                                <input type="text" id="tag-en" name="trans[en][tags]" class="form-control" maxlength="200" data-rule-required="true" data-role="tagsinput" value="{{ old('trans.en.tags') }}">
-                                @include('elements.error_line', ['attribute' => 'trans.en.tags'])
-                            </div>
-                        </div>
+            {{ csrf_field() }}
+            <div class="col-md-8">
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Basic information</h3>
                     </div>
-                </form>
-            </div>
 
-            <div class="box-footer">
-                <div class="button-list col-lg-12">
-                    <div class="col-lg-8 col-lg-offset-2">
-                        <div class="col-lg-3">
-                            <button class="btn btn-block btn-default" form="create-new-product" type="submit">Create</button>
-                        </div>
-                        <div class="col-lg-offset-1 col-lg-3">
-                            <button class="btn btn-block btn-default" form="create-new-product" type="reset">Reset</button>
-                        </div>
-                        <div class="col-lg-offset-1 col-lg-3">
-                            <a href="{{ route('product.index') }}" class="btn btn-block btn-default">Back</a>
+                    <div class="box-body with-border">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <label class="col-sm-3 control-label" for="title-en"> Title <span class="span-red">*</span></label>
+                                <div class="col-sm-9">
+                                    <input type="text" id="title-en" name="trans[en][title]" data-rule-required="true" maxlength="200" class="form-control" placeholder="Title ..." value="{{ old('trans.en.title') }}">
+                                    @include('elements.error_line', ['attribute' => 'trans.en.title'])
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-3 control-label" for="slug-en"> Slug <span class="span-red">*</span></label>
+                                <div class="col-sm-9">
+                                    <input type="text" id="slug-en" name="trans[en][slug]" data-rule-required="true" maxlength="200" class="form-control" value="{{ old('trans.en.slug') }}">
+                                    @include('elements.error_line', ['attribute' => 'trans.en.slug'])
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-3 control-label" for="des_ckediter"> Description <span class="span-red">*</span></label>
+                                <div class="col-sm-9">
+                                    <textarea class="form-control" id="des_ckediter" name="trans[en][description]" maxlength="1000" data-rule-required="true" rows="4" cols="80">{{ old('trans.en.description') }}</textarea>
+                                    @include('elements.error_line', ['attribute' => 'trans.en.description'])
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-3 control-label" for="content_ckediter"> Content <span class="span-red">*</span></label>
+                                <div class="col-sm-9">
+                                    <textarea class="form-control" id="content_ckediter" name="trans[en][content]" maxlength="20000" rows="10" cols="80">{{ old('trans.en.content') }}</textarea>
+                                    @include('elements.error_line', ['attribute' => 'trans.en.content'])
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-3 control-label" for="tag-en"> Tag <span class="span-red">*</span></label>
+                                <div class="col-sm-9">
+                                    <input type="text" id="tag-en" name="trans[en][tags]" class="form-control" maxlength="200" data-rule-required="true" data-role="tagsinput" value="{{ old('trans.en.tags') }}">
+                                    @include('elements.error_line', ['attribute' => 'trans.en.tags'])
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-md-4">
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Publish date</h3>
+                    </div>
+                    <div class="box-body with-border">
+                    </div>
+                </div>
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">End date</h3>
+                    </div>
+                    <div class="box-body with-border">
+                    </div>
+                </div>
+            </div>
 
-        </div>
+            <div class="button-list col-lg-12">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <div class="col-lg-3">
+                        <button class="btn btn-block btn-default" form="create-new-product" type="submit">Create</button>
+                    </div>
+                    <div class="col-lg-offset-1 col-lg-3">
+                        <button class="btn btn-block btn-default" form="create-new-product" type="reset">Reset</button>
+                    </div>
+                    <div class="col-lg-offset-1 col-lg-3">
+                        <a href="{{ route('product.index') }}" class="btn btn-block btn-default">Back</a>
+                    </div>
+                </div>
+            </div>
+        </form>
     </section>
+    <div class="clearfix"></div>
 @endsection
 
 @section('script')
